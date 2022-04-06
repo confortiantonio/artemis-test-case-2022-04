ce4db906db6c004943d0a6b249893ff22ab4eeac
confortiantonio <49157784+confortiantonio@users.noreply.github.com>
2022-04-06 15:54:04 +0200

    Update main.cpp
    elminato sleep in test case lvq step 4

--- test-case-lvq/step4/tickerplan-prd2/main.cpp
+++ test-case-lvq/step4/tickerplan-prd2/main.cpp
@@ -177,7 +177,7 @@ public:
                 message->setStringProperty("MESSAGE_KEY", "1000");                
                 producer->send(message.get());
                 printf("Sent message #%d\n", ix);
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                //std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
 
         }
