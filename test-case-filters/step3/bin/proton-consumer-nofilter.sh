11010a925f8cc78757d817ef11b6a7c7ae9996b8
confortiantonio <49157784+confortiantonio@users.noreply.github.com>
2022-04-27 17:32:52 +0200

    Add files via upload

---
+++ test-case-filters/step3/bin/proton-consumer-nofilter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/sviluppo/repo-git/prometheus-cpp/_build/deploy/usr/local/lib64:/sviluppo/repo-git/qpid-proton-0.33.0/build/cpp
+
+#FILTER="bv.%.%.id"
+FILTER="bv.%.1.id"
+URL="//127.0.0.1:61616"
+ADDRESS="SELECTOR::QUEUE.SEL"
+
+../proton-consumer/bin/x64/Debug/proton-consumer.out $URL  $ADDRESS 
---
+++ test-case-filters/step3/bin/push-srv-proton-prd-selector.sh
@@ -0,0 +1,1 @@
+../push-srv-proton-prd/bin/x64/Debug/push-srv-proton-prd.out -i 100 -t 60000 -a 127.0.0.1:61616/SELECTOR
